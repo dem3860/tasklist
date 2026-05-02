bb524c1266afb7c3a86e9d78ec756da394e8e600
ydemu <y.demuchan3860@gmail.com>
2026-05-02 09:03:33 +0900

    PostMapping,GetMappingを使用するように修正

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,8 +1,6 @@
 package jp.gihyo.projava.tasklist;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,7 +13,7 @@ public class HomeRestController {
     record  TaskItem(String id,String task,String deadline,boolean done) {}
     private List<TaskItem> taskItems = new ArrayList<>();
 
-    @RequestMapping(value="/resthello")
+    @GetMapping(value="/resthello")
     String hello() {
         return """
                 Hello.
@@ -24,17 +22,17 @@ public class HomeRestController {
                 """.formatted(LocalDateTime.now());
     }
 
-    @RequestMapping("/restadd")
-    String addItem(@RequestParam("task") String task,
+    @PostMapping("/restadd")
+    TaskItem addItem(@RequestParam("task") String task,
                    @RequestParam("deadline") String deadline) {
         String id = UUID.randomUUID().toString().substring(0,8);
         TaskItem item = new TaskItem(id,task,deadline,false);
         taskItems.add(item);
 
-        return "タスクを追加しました";
+        return item;
     }
 
-    @RequestMapping("/restlist")
+    @GetMapping("/restlist")
     String listItems() {
         return taskItems.stream()
                 .map(TaskItem::toString)
